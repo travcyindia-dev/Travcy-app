9de8fb18e049fb7a719eac0e9cf2791b255f150f
SAM324674 <samikshagiri4@gmail.com>
2025-12-15 14:36:11 +0530

    build issue fixed

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -1,29 +1,11 @@
 import admin from "firebase-admin";
+import serviceAccountJson from "@/firebase-upload/serviceAccountKey.json"; // rename import
 
-if (!admin.apps.length) {
-  // Join all chunks into one Base64 string
-  const serviceAccountBase64 = [
-    process.env.FIREBASE_CHUNK_1,
-    process.env.FIREBASE_CHUNK_2,
-    process.env.FIREBASE_CHUNK_3,
-    process.env.FIREBASE_CHUNK_4,
-    process.env.FIREBASE_CHUNK_5,
-    process.env.FIREBASE_CHUNK_6,
-    process.env.FIREBASE_CHUNK_7,
-   // add more if you have more chunks
-  ]
-    .filter(Boolean) // ignore any undefined
-    .join("");
+const serviceAccount: admin.ServiceAccount = serviceAccountJson as admin.ServiceAccount;
 
-  if (!serviceAccountBase64) throw new Error("Firebase service account Base64 not set!");
-
-  const serviceAccount = JSON.parse(
-    Buffer.from(serviceAccountBase64, "base64").toString("utf-8")
-  );
-
+if(!admin.apps.length){
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
+  credential: admin.credential.cert(serviceAccount),
+});
 }
-
-export { admin };
+export {admin}
